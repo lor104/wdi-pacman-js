bd8e34b9fcb996cbb771fff31bdab3510b845f22
lor104 <lauren.rosentzveig@gmail.com>
2017-04-26 15:37:11 -0400

    remove power pellets from the menu if there are none left

--- pacman.js
+++ pacman.js
@@ -63,8 +63,9 @@ function displayStats() {
 function displayMenu() {
   console.log('\n\nSelect Option:\n');  // each \n creates a new line
   console.log('(d) Eat Dot');
-  console.log('(p) Eat Power Pellet');
-  console.log('(1) Eat Inky');
+  if (powerPellets >= 1) {
+    console.log('(p) Eat Power Pellet');
+} console.log('(1) Eat Inky');
   console.log('(2) Eat Blinky');
   console.log('(3) Eat Pinky');
   console.log('(4) Eat Clyde');
